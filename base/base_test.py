b1f59f92f0a8acd8d9c19128b2a0c29cf1e3587d
ErionPham-96 <phamcao.nam1996@gmail.com>
2025-09-20 19:35:33 +0700

    Github actions fix v3

--- base/base_test.py
+++ base/base_test.py
@@ -16,11 +16,11 @@ class BaseTest:
     @pytest.fixture(scope="class", autouse=True)
     
     def setup_driver(self, request, config):
-        options = Options()
+        options = webdriver.ChromeOptions()
         options.add_argument("--headless")          # chạy không bật UI
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options = options)
         driver.maximize_window()
         
         driver.get(config["base_url"])
