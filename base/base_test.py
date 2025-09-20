86156c954661e1264cb61f3ef0e757349feda94a
ErionPham-96 <phamcao.nam1996@gmail.com>
2025-09-20 20:14:31 +0700

    fixed version khong nho

--- base/base_test.py
+++ base/base_test.py
@@ -20,7 +20,7 @@ class BaseTest:
         options.add_argument("--headless")         
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options=options)
         driver.maximize_window()
         
         driver.get(config["base_url"])
